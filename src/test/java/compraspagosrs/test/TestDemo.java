91b084844e6cd76d7eea7d82064f645941e50608
WILLIAMS VALER <wcvaler@pe.ibm.com>
2018-06-15 10:21:01 -0500

    test pruebas

---
+++ src/test/java/compraspagosrs/test/TestDemo.java
@@ -0,0 +1,14 @@
+package compraspagosrs.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestDemo {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
